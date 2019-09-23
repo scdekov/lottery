ac09b1fafc117bc72d7d0fd5ce1e51ab9ccd115b
svetoslav dekov <svetoslav.dekov@mentormate.com>
2019-09-23 17:22:16 +0300

    test hardcoded winner

--- lottery/models.py
+++ lottery/models.py
@@ -20,6 +20,7 @@ class Game(models.Model):
                 self.winning_numbers.append(number)
 
         # self.is_active = False
+        self.winning_numbers = [3, 31, 15, 5, 22, 31]
         self.save()
 
     @property
